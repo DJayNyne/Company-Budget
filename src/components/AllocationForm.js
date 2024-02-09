45c736d146831c273daa66a29a33c0cf361654f3
DJayNyne <157189086+DJayNyne@users.noreply.github.com>
2024-02-09 06:29:05 -0700

    Update AllocationForm.js

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react';
-import { AppContext } from '../context/AppContext';
+import { AppContext } from '../../../ejtos-react_budget_app/src/context/AppContext';
 
 const AllocationForm = (props) => {
     const { dispatch,remaining  } = useContext(AppContext);
