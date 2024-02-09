412493a76e5340d0acdd515a4c3c02b7c6218980
DJayNyne <157189086+DJayNyne@users.noreply.github.com>
2024-02-09 06:30:08 -0700

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
-import { AppContext } from '../context/AppContext';
+import { AppContext } from '../../../ejtos-react_budget_app/src/context/AppContext';
 
 const ExpenseItem = (props) => {
     const { dispatch } = useContext(AppContext);
@@ -27,10 +27,10 @@ const ExpenseItem = (props) => {
 
     return (
         <tr>
-            <td>{props.name}</td>
-            <td>£{props.cost}</td>
-            <td><button onCLick={event=> increaseAllocation(props.name)}>+</button></td>
-            <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+        <td>{props.name}</td>
+        <td>£{props.cost}</td>
+        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
 };
